125188e2503c1d3c84e78f8adf45a74b84c4989a
Nikhil Dixit <170299140+nikhildixit-321@users.noreply.github.com>
2026-02-25 17:08:50 +0530

    test2

--- backend/index.js
+++ backend/index.js
@@ -5,5 +5,13 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const axios = require('axios')
 const app = express()
-sdfs
-fsdsd fsdff
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Ticket Counter API!")
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`)
+})
