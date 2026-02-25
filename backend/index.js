53349b8c40306d2ea7fc4a2726c4a2f31353d906
nikhil <nikhildixit525@gmail.com>
2026-02-25 12:35:30 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config()  
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const axios = require('axios')
